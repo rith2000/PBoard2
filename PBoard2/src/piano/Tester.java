1620a2603cf45ee6eebc0643fb28cbf9eab9bb6d
amandasun <amandasun@172.31.98.233>
2018-05-05 16:36:30 -0700

    added BufferedReader in Tester class

--- PBoard2/src/piano/Tester.java
+++ PBoard2/src/piano/Tester.java
@@ -1,8 +1,25 @@
 package piano;
 
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Tester {
-	public static void main(String[] args) {
-		System.out.print("x");
+	public static void main(String[] args) throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		String line = "";
+
+		   while (line.equalsIgnoreCase("quit") == false) {
+		       line = in.readLine();
+		       if (line.equals("S")) {
+		    	   System.out.print("pressed");
+		    	   SoundClipTest newSound = new SoundClipTest();
+		    	   newSound.playSound();
+		       }
+		       //do something
+		   }
+
+		   in.close();
 	}
 }
