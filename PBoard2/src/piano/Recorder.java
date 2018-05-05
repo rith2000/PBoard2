43b1511379d451a239e82a9b8d9902f057e0e721
rithika <rithika@172.31.98.203>
2018-05-05 16:45:13 -0700

    Recorder change 1

--- PBoard2/src/piano/Recorder.java
+++ PBoard2/src/piano/Recorder.java
@@ -3,4 +3,17 @@ package piano;
 
 public class Recorder {
 
+	private static int MEASURE_NUMBER;
+	private final int BEATS_PER_MEASURE;
+
+	
+	public Recorder(int beats) {
+		BEATS_PER_MEASURE = beats;
+
+	}
+	
+	public void record(Piano p, int measNum, int beat) {
+		
+	}
+
 }
--- PBoard2/src/piano/Tester.java
+++ PBoard2/src/piano/Tester.java
@@ -1,8 +1,18 @@
 package piano;
 
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.Scanner;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class Tester {
-	public static void main(String[] args) {
-		System.out.print("x");
-	}
-}
+	
+	public static void main(String[] args)
+	  {
+	
+		
+	  }
+
+}	
