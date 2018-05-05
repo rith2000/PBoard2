4fadd44e283dfc47e76125101857275133f58bef
KeertanaMacAir <KeertanaMacAir@172.31.98.237>
2018-05-05 16:40:01 -0700

    new changes to tester class

--- PBoard2/src/piano/Tester.java
+++ PBoard2/src/piano/Tester.java
@@ -1,8 +1,16 @@
 package piano;
 
+import java.util.Scanner;
 
 public class Tester {
 	public static void main(String[] args) {
-		System.out.print("x");
+		Scanner kboard = new Scanner(System.in);
+	
+		String key = kboard.nextLine();
+		if(key =="s") {
+			System.out.println("c");
+		}
+		
+		
 	}
 }
